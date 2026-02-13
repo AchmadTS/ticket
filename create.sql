7aec4db8a9b63c2e844f91bd5911d384b09b3353
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2026-02-13 15:35:05 +0700

    SG #6 BackEnd

---
+++ create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Customers (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  city TEXT NOT NULL
+);
+
+CREATE TABLE Orders (
+  id SERIAL PRIMARY KEY,
+  course TEXT NOT NULL,
+  price INT NOT NULL,
+  customer_id INT REFERENCES customers(id),
+  order_date DATE
+);
